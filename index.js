d4b9a2cafdc48e81147132c4b0f8a2519d70b19e
Andrew Robbins <arobbins@simpleblend.net>
2020-08-10 11:45:00 -0500

    Updated dev deps

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { Icon } from './blocks/_icons'
 export { registerBlockProducts } from './blocks/products'
 export { registerBlockSingleProduct } from './blocks/single-product'
 export { registerBlockBuyButton } from './blocks/buy-button'
+
+wp.blocks.updateCategory('wpshopify-products', { icon: Icon })
